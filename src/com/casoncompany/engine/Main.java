17da142996a2a6412964b6f82e251fe07022cebc
Gregory Brinson <casonbrinson@vt.edu>
2022-05-15 00:33:49 -0400

    added the window class and game loop setup

--- src/com/casoncompany/engine/Main.java
+++ src/com/casoncompany/engine/Main.java
@@ -1,13 +1,22 @@
 package com.casoncompany.engine;
 
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+
 public class Main implements Runnable {
 	
 	private static final int UPDATES_PER_SECOND = 60;
 	private static final double NANOSECONDS_PER_UPDATE = 1000000000.0 / UPDATES_PER_SECOND;
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+	private static final String TITLE = "3D Game Engine";
 	
 	private Thread thread;
 	private boolean running = false;
 	
+	private Window window;
+	private GLFWErrorCallback errorCallback;
+		
 	public void start() {
 		if(running)return;
 		
@@ -22,6 +31,7 @@ public class Main implements Runnable {
 		
 		running = false;
 		
+		window.cleanup();
 		try {
 			thread.join();
 		} catch(Exception e) {
@@ -32,7 +42,13 @@ public class Main implements Runnable {
 	}
 	
 	public void init() {
-		System.out.println("Initializing game...");
+		System.out.println("Initializing game");
+		
+		window = new Window(WIDTH, HEIGHT, TITLE);
+		window.init();
+		
+		errorCallback = GLFWErrorCallback.createPrint(System.err);
+		GLFW.glfwSetErrorCallback(errorCallback);
 	}
 	
 	//draws the game to the screen
@@ -42,37 +58,41 @@ public class Main implements Runnable {
 	
 	//updates the state of things in the game at a consistent rate of UPDATES_PER_SECOND
 	public void update() {
-		
+		window.update();
 	}
 	
+	//TODO fix this broke ass loop
 	public void loop() {
 		System.out.println("Starting game loop");
-		long prevTime = System.nanoTime();
-		long timer = System.currentTimeMillis();
-		int frames = 0;
-		double delta = 0.0;	//when delta hits a value of 1 (100%) then we should do an update
-		
-		
-		while(running) {
-			long currTime = System.nanoTime();
-			delta += (currTime - prevTime) / NANOSECONDS_PER_UPDATE;
-			prevTime = currTime;
-			
-			while(delta >= 1) {
-				update();
-				delta-=1.0;
-			}
-			
-			render();
-			frames++;
-			
-			//if a second has passed
-			if((System.currentTimeMillis() - timer) > 1000) {
-				timer+=1000;
-				//gameWindow.changeTitle(TITLE + " - " + frames + " fps");
-				frames = 0;
-			}
-		}
+//		long prevTime = System.nanoTime();
+//		long timer = System.currentTimeMillis();
+//		int frames = 0;
+//		double delta = 0.0;	//when delta hits a value of 1 (100%) then we should do an update
+//		
+//		
+//		while(running) {
+//			long currTime = System.nanoTime();
+//			delta += (currTime - prevTime) / NANOSECONDS_PER_UPDATE;
+//			prevTime = currTime;
+//			
+//			while(delta >= 1) {
+//				if(window.windowShouldClose())
+//					stop();
+//				
+//				update();
+//				delta = delta - 1.0;
+//			}
+//			
+//			render();
+//			frames++;
+//			
+//			//if a second has passed
+//			if((System.currentTimeMillis() - timer) > 1000) {
+//				timer+=1000;
+//				System.out.println(TITLE + " - " + frames + " fps");
+//				frames = 0;
+//			}
+//		}
 	}
 
 	@Override
--- src/com/casoncompany/engine/Window.java
+++ src/com/casoncompany/engine/Window.java
@@ -1,5 +1,86 @@
 package com.casoncompany.engine;
 
-public class Window {
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.system.MemoryUtil;
 
+import org.joml.Matrix4f;
+
+public class Window {
+	
+	private static final float FOV = (float) Math.toRadians(60);
+	private static final float Z_NEAR = 0.01f;
+	private static final float Z_FAR = 1000f;
+	
+	private final Matrix4f projectionMatrix;
+	
+	private int width;
+	private int height;
+	private String title;
+	
+	private long window;
+	
+	public Window(int width, int height, String title) {
+		this.width = width;
+		this.height = height;
+		this.title = title;
+		projectionMatrix = new Matrix4f();
+	}
+	
+	public void init() {
+		if(!GLFW.glfwInit())
+			throw new IllegalStateException("Unable to initialize GLFW");
+		
+		GLFW.glfwDefaultWindowHints();
+		GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE); 
+		GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
+		
+		window = GLFW.glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
+		if(window == MemoryUtil.NULL)
+			throw new RuntimeException("Failed to create the GLFW window");
+		
+		GLFWVidMode vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+		GLFW.glfwSetWindowPos(window, (vidmode.width() - width) / 2, (vidmode.height() - height) / 2);
+		
+		GLFW.glfwMakeContextCurrent(window);
+		GLFW.glfwSwapInterval(1);	//vsync?
+		
+		GLFW.glfwShowWindow(window);
+		GL.createCapabilities();
+		GL11.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);			
+	}
+	
+	public void update() {
+		GLFW.glfwSwapBuffers(window);
+		GLFW.glfwPollEvents();
+	}
+	
+	public void cleanup() {	
+		GLFW.glfwDestroyWindow(window);
+	}
+	
+	public boolean windowShouldClose() {
+		return GLFW.glfwWindowShouldClose(window);
+	}
+	
+	public void setTitle(String title) {
+		GLFW.glfwSetWindowTitle(window, title);
+	}
+	
+	public Matrix4f getProjectionMatrix() {
+		return projectionMatrix;
+	}
+	
+	public Matrix4f updateProjectionMatrix() {
+		float aspectRatio = (float) width / height;
+		return projectionMatrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+	}
+	
+	public Matrix4f updateProjectionMatrix(Matrix4f matrix, int width, int height) {
+		float aspectRatio = (float) width / height;
+		return matrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+	}
 }
